12fd2f1f88415d48941b6df82f57538597482c3a
Raissa <raissa.feitosa06@gmail.com>
2026-07-09 22:22:56 -0300

    feat: cria página de Categoria

---
+++ src/pages/categoria/Categoria.tsx
@@ -0,0 +1,26 @@
+import ListaCategorias from "../../componentes/categoria/listarcategorias/ListarCategorias";
+import { Link } from "react-router-dom";
+
+function Categorias() {
+  return (
+    <div className="container mx-auto my-12 px-4">
+      
+      <div className="flex justify-end mb-8">
+        <Link 
+          to="/cadastrarcategoria" 
+          className="bg-[var(--color-red)] hover:bg-red-700 text-white font-bold py-2 px-3 text-sm rounded-full transition-all shadow-lg whitespace-nowrap"
+        >
+          + Nova Categoria
+        </Link>
+      </div>
+
+      <div className="text-center mb-12">
+        <h1 className="text-4xl font-black text-slate-800">Gestão de Categorias</h1>
+        <p className="text-slate-500 mt-2">Adicione, edite ou remova categorias do cardápio</p>
+      </div>
+      
+      <ListaCategorias />
+    </div>
+  );
+}
+export default Categorias;
