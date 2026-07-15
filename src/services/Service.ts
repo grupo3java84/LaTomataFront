45d0f22488a337dfce6382081e8531c68321a76a
Dayana Barbosa <day_sbarbosa@outlook.com>
2026-07-15 15:34:47 -0300

    Feat: Inclusão de categoria

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -13,3 +13,28 @@ export const login = async (url: string, dados: Object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
 }
+
+export const listarCategorias = async (setDados: Function, header: Object) => {
+  const resposta = await api.get('/categorias', header)
+  setDados(resposta.data)
+}
+
+export const buscarCategoria = async (nome: string, setDados: Function, header: Object) => {
+  const resposta = await api.get(`/categorias/nome/${nome}`, header)
+  setDados(resposta.data)
+}
+
+
+export const cadastrarCategoria = async (dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.post('/categorias', dados, header)
+  setDados(resposta.data)
+}
+
+export const atualizarCategoria = async (dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.put('/categorias', dados, header)
+  setDados(resposta.data)
+}
+
+export const deletarCategoria = async (id: string, header: Object) => {
+  await api.delete(`/categorias/${id}`, header)
+}
